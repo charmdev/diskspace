18f35fd226d0329be005fcd188a60ec9e053b1be
Александр Хохлов <aleksandr.khokhlov@nevosoft.ru>
2020-05-28 15:49:17 +0400

    implementation for iOS (first try)

--- project/common/DiskSpace.mm
+++ project/common/DiskSpace.mm
@@ -7,6 +7,22 @@ namespace diskspace
 {	
 	bool GetIsAvailable(int requiredSpace) 
 	{
-		return true;
+		uint64_t totalFreeSpace = 0;
+		NSError *error = nil;  
+		NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);  
+		NSDictionary *dictionary = [[NSFileManager defaultManager] attributesOfFileSystemForPath:[paths lastObject] error: &error];  
+
+		if (dictionary) 
+		{  
+			NSNumber *fileSystemSizeInBytes = [dictionary objectForKey: NSFileSystemSize];  
+			NSNumber *freeFileSystemSizeInBytes = [dictionary objectForKey:NSFileSystemFreeSize];
+			totalFreeSpace = [freeFileSystemSizeInBytes unsignedLongLongValue];
+		} 
+		else 
+		{  
+			NSLog(@"Error Obtaining System Memory Info: Domain = %@, Code = %ld", [error domain], (long)[error code]);
+		}  
+
+		return (totalFreeSpace > requiredSpace);
 	}
 }
